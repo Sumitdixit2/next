d94735aa9a5d8407e044f986455d78b1824906dd
Sumitdixit2 <sumitdixit00007@gmail.com>
2026-01-04 10:50:45 +0530

    sample

--- components/MessageCard.tsx
+++ components/MessageCard.tsx
@@ -23,6 +23,8 @@ import {
 import { Button } from "./ui/button";
 import { X } from "lucide-react";
 import { Message } from "@/src/model/User.model";
+import { ApiResponse } from "@/src/types/ApiResponse";
+import
 
 type MessageCardProps = {
   message: Message;
@@ -30,6 +32,10 @@ type MessageCardProps = {
 };
 
 function MessageCard({ message, onMessageDelete }: MessageCardProps) {
+  
+  const handleDelete = async () => {
+    axios.delete<ApiResponse>(`/api/delete-message/${}`)
+  }
   return (
     <Card>
       <CardHeader>
@@ -45,6 +51,7 @@ function MessageCard({ message, onMessageDelete }: MessageCardProps) {
               <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
               <AlertDialogDescription>
                 This action cannot be undone. This will permanently delete your
+
                 account and remove your data from our servers.
               </AlertDialogDescription>
             </AlertDialogHeader>
